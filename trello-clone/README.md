3cb91410aadc6fb1eba4afffce624e0da84839ec
uiseop <bono521@naver.com>
2022-01-20 22:04:47 +0900

    react-hook-form 으로 Form의 validation 및 error처리

--- trello-clone/README.md
+++ trello-clone/README.md
@@ -1,21 +1,45 @@
 # Recoil 심화 학습
 
 ## Form태그 처리하기
+
 Form태그를 처리하기 위해서는 입력 값들을 저장하는 여러개의 `State`가 필요하고, 그와 동시에 `setState`를 처리하며, `onSubmit함수`도 제공해야 합니다.
 
-한 페이지 안에 `여러개의 Form`이 있으면 여러개의 단순한 동일 동작을 해야할것입니다. 
+한 페이지 안에 `여러개의 Form`이 있으면 여러개의 단순한 동일 동작을 해야할것입니다.
 
 ## React Hook Form 라이브러리
-- 리액트에서 form 작업을 매우 처리하기 쉽게 해준다. `단연코 최고`라고 한다.
+
+-   리액트에서 form 작업을 매우 처리하기 쉽게 해준다. `단연코 최고`라고 한다.
 
 위의 모든 과정을 단 한줄로 끝내준다고 합니다.
+
 ```
 npm i react-hook-form
-const {register, watch} = useForm();
+const { register, watch, handleSubmit } = useForm();
 
 <input {...register("email")}/>
 <input {...register("firstName")}/>
 <input {...register("lastName")}/>
 <input {...register("password")}/>
 ```
-useForm의 register함수와 watch함수를 사용해서 하나 하나 `useState`, `setState`를 설정해주지 않아도 `onChange`, `onBlur` 등의 함수가 내장되어서 훨 씬 쉽고 간결하게 코드를 짤 수 있게 도와줍니다. 
+
+-   useForm의 register함수와 watch함수를 사용해서 하나 하나 `useState`, `setState`를 설정해주지 않아도 `onChange`, `onBlur` 등의 함수가 내장되어서 훨 씬 쉽고 간결하게 코드를 짤 수 있게 도와줍니다.
+-   `handleSubmit`은 onSubmit을 담당합니다. (다있네유 증말루)
+
+```
+handleSubmit(onValid함수, inValid함수(필수x))
+```
+
+위 코드처럼 작성하면 되고, onValid함수는 입력타입이 `Valid할 경우`에 실행되는 함수입니다.
+가령
+
+```
+<input {...register("email", { required: true, minLenght: 10 })}/>
+<input {...register("password1", { required: "Password is required", minLenght: 10 })}/>
+<input {...register("password2", { 
+    required: true, 
+    minLenght: {
+    value: 10,
+    message: "Your password is too short! })}/>
+```
+
+이처럼 작성되어있으면 `input필드`가 비어있으면 inValid함수가 실행되면서 어디서 오류가 발생했는지 알려줘.(Input태그의 required를 사용하지 않는 이유는, 사용자가 임의로 그 코드를 지우면 HTML이 valid를 보장해주지 못해, 혹은 required를 지원하지 않는 브라우저에서의 문제도 있음)
--- trello-clone/src/ToDoList.tsx
+++ trello-clone/src/ToDoList.tsx
@@ -1,18 +1,80 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
-function ToDoList() {
-    const { register, watch } = useForm();
-    console.log(watch())
-
+interface IForm {
+    email: string;
+    firstName: string;
+    lastName: string;
+    username: string;
+    password: string;
+    password1: string;
+  }
+  
+  function ToDoList() {
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm<IForm>({
+      defaultValues: {
+        email: "@naver.com",
+      },
+    });
+    const onValid = (data: any) => {
+      console.log(data);
+    };
     return (
-        <div>
-            <form>
-                <input {...register("todo")} type="text" placeholder="Write a to do" />
-                <button>Add</button>
-            </form>
-        </div>
+      <div>
+        <form
+          style={{ display: "flex", flexDirection: "column" }}
+          onSubmit={handleSubmit(onValid)}
+        >
+          <input
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+                message: "Only naver.com emails allowed",
+              },
+            })}
+            placeholder="Email"
+          />
+          <span>{errors?.email?.message}</span>
+          <input
+            {...register("firstName", { required: "write here" })}
+            placeholder="First Name"
+          />
+          <span>{errors?.firstName?.message}</span>
+          <input
+            {...register("lastName", { required: "write here" })}
+            placeholder="Last Name"
+          />
+          <span>{errors?.lastName?.message}</span>
+          <input
+            {...register("username", { required: "write here", minLength: 10 })}
+            placeholder="Username"
+          />
+          <span>{errors?.username?.message}</span>
+          <input
+            {...register("password", { required: "write here", minLength: 5 })}
+            placeholder="Password"
+          />
+          <span>{errors?.password?.message}</span>
+          <input
+            {...register("password1", {
+              required: "Password is required",
+              minLength: {
+                value: 5,
+                message: "Your password is too short.",
+              },
+            })}
+            placeholder="Password1"
+          />
+          <span>{errors?.password1?.message}</span>
+          <button>Add</button>
+        </form>
+      </div>
     );
-}
-
+  }
+  
 export default ToDoList;
