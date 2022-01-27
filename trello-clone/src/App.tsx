9bc6c851def2f30601e50d75db17b4bd0e7b8c85
uiseop <bono521@naver.com>
2022-01-27 09:39:46 +0900

    Drag => 함수와 객체 넘겨주기

--- trello-clone/src/App.tsx
+++ trello-clone/src/App.tsx
@@ -1,60 +1,7 @@
-import React from "react";
-import { RecoilRoot } from "recoil";
-import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
-import { darkTheme } from "./theme";
-import ToDoList from "./components/ToDoList";
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
-
-const GlobalStyle = createGlobalStyle`
-  html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, menu, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed,
-figure, figcaption, footer, header, hgroup,
-main, menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-  margin: 0;
-  padding: 0;
-  border: 0;
-  font-size: 100%;
-  font: inherit;
-  vertical-align: baseline;
-}
-/* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure,
-footer, header, hgroup, main, menu, nav, section {
-  display: block;
-}
-/* HTML5 hidden-attribute fix for newer browsers */
-*[hidden] {
-    display: none;
-}
-body {
-  line-height: 1;
-  background: ${(props) => props.theme.bgColor};
-}
-menu, ol, ul {
-  list-style: none;
-}
-blockquote, q {
-  quotes: none;
-}
-blockquote:before, blockquote:after,
-q:before, q:after {
-  content: '';
-  content: none;
-}
-table {
-  border-collapse: collapse;
-  border-spacing: 0;
-}
-`;
+import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
+import styled from "styled-components";
+import { DragDropContext, Draggable, Droppable, DropResult } from "react-beautiful-dnd";
+import { toDoState } from "./atoms";
 
 const Wrapper = styled.div`
     display: flex;
@@ -87,49 +34,46 @@ const Card = styled.div`
     background-color: ${(props) => props.theme.cardColor};
 `;
 
-const toDos = ["a", "b", "c", "d", "e", "f"];
-
 function App() {
-    const onDragEnd = () => {};
+    const [toDos, setTodos] = useRecoilState(toDoState);
+    const onDragEnd = ({ destination, source }: DropResult) => {
+    };
     return (
         <>
-            <RecoilRoot>
-                <ThemeProvider theme={darkTheme}>
-                    <GlobalStyle />
-                    <DragDropContext onDragEnd={onDragEnd}>
-                        <Wrapper>
-                            <Boards>
-                                <Droppable droppableId="one">
-                                    {(magic) => (
-                                        <Board
-                                            ref={magic.innerRef}
-                                            {...magic.droppableProps}
+            <DragDropContext onDragEnd={onDragEnd}>
+                <Wrapper>
+                    <Boards>
+                        <Droppable droppableId="one">
+                            {(magic) => (
+                                <Board
+                                    ref={magic.innerRef}
+                                    {...magic.droppableProps}
+                                >
+                                    {toDos.map((toDo, index) => (
+                                        <Draggable
+                                            key={index}
+                                            draggableId={toDo}
+                                            index={index}
                                         >
-                                            {toDos.map((toDo, index) => (
-                                                <Draggable
-                                                    draggableId={toDo}
-                                                    index={index}
+                                            {(magic) => (
+                                                <Card
+                                                    ref={magic.innerRef}
+                                                    {...magic.dragHandleProps}
+                                                    {...magic.draggableProps}
                                                 >
-                                                    {(magic) => (
-                                                        <Card
-                                                            ref={magic.innerRef}
-                                                            {...magic.dragHandleProps}
-                                                            {...magic.draggableProps}
-                                                        >
-                                                            {toDo}
-                                                        </Card>
-                                                    )}
-                                                </Draggable>
-                                            ))}
-                                            {magic.placeholder}
-                                        </Board>
-                                    )}
-                                </Droppable>
-                            </Boards>
-                        </Wrapper>
-                    </DragDropContext>
-                </ThemeProvider>
-            </RecoilRoot>
+                                                    {toDo}
+                                                </Card>
+                                            )}
+                                        </Draggable>
+                                    ))}
+                                    {magic.placeholder}
+                                    {/* magic.placeholder를 사용해서 카드를 빼도 크기 변화 x */}
+                                </Board>
+                            )}
+                        </Droppable>
+                    </Boards>
+                </Wrapper>
+            </DragDropContext>
         </>
     );
 }
--- trello-clone/src/atoms.tsx
+++ trello-clone/src/atoms.tsx
@@ -1,37 +1,7 @@
 import { atom, selector } from "recoil";
 
-export enum Categories {
-    "TO_DO" = "TO_DO",
-    "DOING" = "DOING",
-    "DONE" = "DONE",
-}
 
-export interface ITodo {
-    text: string;
-    id: number;
-    category: Categories; // 3가지 종류만 쓸 수 있음
-}
-
-export const categoryState = atom<Categories>({
-    key: "category", 
-    default: Categories.TO_DO,
-});
-
-export const toDoState = atom<ITodo[]>({
+export const toDoState = atom({
     key: "toDo",
-    default: [],
-});
-
-export const toDoSelector = selector({
-    key: "toDoSelector",
-    get: ({ get }) => {
-        const todos = get(toDoState);
-        const category = get(categoryState);
-        if (category === Categories.TO_DO)
-            return todos.filter((todo) => todo.category === Categories.TO_DO);
-        if (category === Categories.DOING)
-            return todos.filter((todo) => todo.category === Categories.DOING);
-        if (category === Categories.DONE)
-            return todos.filter((todo) => todo.category === Categories.DONE);
-    },
+    default: ["a", "b", "c", "d", "e", "f"],
 });
